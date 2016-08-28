80e76e2d0eb6854f07d6da76e107de8fcc6ebcbf
Paulo Kanayama <Paulo Kanayama>
2016-08-28 19:53:46 -0300

    first commit

---
+++ FarmCabal/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Interceptor;
+
+namespace FarmCabal
+{
+    public partial class Form1 : Form
+    {
+        private bool start;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Clear();
+        }
+
+        public static void MySleep(int milesegundos)
+        {
+            DateTime dt = DateTime.Now + TimeSpan.FromMilliseconds(milesegundos);
+
+            do { } while (DateTime.Now < dt);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                //Input input = new Input();
+                this.start = true;
+                int countIndex = dataGridView1.RowCount - 1;
+                int index = 0;
+                dataGridView1.Rows[0].Selected = true;
+
+                while (this.start)
+                {
+                    string _tecla;
+                    int _milesegundos;
+
+                    _tecla = "{" + dataGridView1.Rows[index].Cells[0].Value.ToString() + "}";
+                    _milesegundos = Convert.ToInt32(dataGridView1.Rows[index].Cells[1].Value);
+
+                    MySleep(_milesegundos);
+
+                    SendKeys.SendWait(_tecla);
+                        //.Send(_tecla);
+
+                    //var hWnd = SendKeySample.FindWindow("Untitled - Notepad");
+                    //var editBox = SendKeySample.FindWindow(hWnd, "edit");
+
+                    //SendKeySample.SendKey(editBox, Keys.A);
+
+                    //input.Load();
+                    //input.SendKeys(Interceptor.Keys.Four);
+                    //input.Unload();
+
+                    if (index >= countIndex)
+                    {
+                        index = 0;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Insira a tecla e os milesegundos para execução!");
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                string[] row = { textBox1.Text, textBox2.Text };
+                dataGridView1.Rows.Add(row);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.start = false;
+        }
+    }
+}
---
+++ FarmCabal/SendKeySample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmCabal
+{
+    public class SendKeySample
+    {
+        private static Int32 WM_KEYDOWN = 0x100;
+        private static Int32 WM_KEYUP = 0x101;
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(IntPtr hWnd, int Msg, System.Windows.Forms.Keys wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        public static IntPtr FindWindow(string windowName)
+        {
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.MainWindowHandle != IntPtr.Zero && p.MainWindowTitle.ToLower() == windowName.ToLower())
+                    return p.MainWindowHandle;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        public static IntPtr FindWindow(IntPtr parent, string childClassName)
+        {
+            return FindWindowEx(parent, IntPtr.Zero, childClassName, string.Empty);
+        }
+
+        public static void SendKey(IntPtr hWnd, System.Windows.Forms.Keys key)
+        {
+            PostMessage(hWnd, WM_KEYDOWN, key, 0);
+
+        }
+    }
+}
